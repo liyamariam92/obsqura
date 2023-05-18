44c1d4c7bb73d3c81477baceee96320894330c63
liyam <liyam@Liya>
2023-05-18 16:37:25 +0530

    commit and push the project to repository

---
+++ obsqura/src/javaobsqura/Reverse.java
@@ -0,0 +1,35 @@
+package javaobsqura;
+
+
+
+public class Reverse {
+	 int num;
+		int r;
+		int rev=0;
+
+		public Reverse(int num) {
+
+			this.num = num;
+			while(num!=0)
+			{
+				r=num%10;
+				rev=rev*10+r;
+				num=num/10;
+			}
+		}
+		
+	public Reverse() {
+			this(245);
+			System.out.println("finding reverse:"+rev);
+		}
+
+	public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			
+	Reverse obj1=new Reverse();
+		
+	}
+
+	}
+
+
